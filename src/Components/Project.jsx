f389dd47bbf773f07623f1243ab58d92633c72b9
Daniel <danielsteinhauser1906@gmail.com>
2021-10-26 11:28:34 +0200

    newtheme1

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -9,7 +9,8 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import { withStyles } from "@material-ui/core/styles"
-
+import {Box }from "@material-ui/core/";
+import Paper from '@material-ui/core/Paper'
 
 import "../App.css"
 
@@ -32,6 +33,17 @@ const Project = ({
           color: "rgb(36, 139, 170)"
         }
       })(Typography)
+
+
+      const bull = (
+        <Box
+          component="span"
+          sx={{ display: 'inline-block', mx: '20px', transform: 'scale(0.8)' }}
+        >
+          •
+        </Box>
+      );
+      
     return (
         // <div classNameName="project" >
         //     <div  className="fadein project-box ">
@@ -52,6 +64,10 @@ const Project = ({
         direction="column"
         justifyContent="flex-start"
         alignItems="center" >
+
+ <Card className="card">
+      <CardContent>
+      
             <Grid  container direction="column" justifyContent="flex-start" alignItems="center" >
             <Grid > <Typography variant="h2"   > Projekt {projectNumber} </Typography></Grid>
                <Grid > <WhiteTextTypography color="inherit" variant="h3" > {projectName} </WhiteTextTypography></Grid>
@@ -59,10 +75,9 @@ const Project = ({
 
            <Grid container spacing={4} direction="row" justifyContent="space-around" alignItems="center">
                 <Grid item xs  md={3}  lg={3}><Typography color="primary" variant="body" >{additionalText}</Typography></Grid>
-                <Grid item xs={12}  md={3}  lg={3}> <a  target="_blank"href={link} >  <img src={image}/></a>   </Grid>
+                <Grid item xs={12}  md={3}  lg={3}> <a  target="_blank"href={link} >  <img className="image" src={image}/></a>   </Grid>
                 <Grid item xs  md={3}  lg={3} >{descriptionList.map((item)=>(<Typography variant="h4" color="secondary" >{item}</Typography>))} </Grid>
-                <Grid  container direction="column" justifyContent="flex-start" alignItems="flex-start"><Button  target="_blank" href={link}  size="large" variant="contained" color="primary"><WhiteTextTypography color="secondary"
-                variant="h3">zum Projekt</WhiteTextTypography>  </Button> 
+                <Grid  container direction="column" justifyContent="flex-start" alignItems="flex-start">
         </Grid>
            </Grid>
            
@@ -70,7 +85,16 @@ const Project = ({
         
 
            
-        </Grid>
+        
+        <CardActions>
+      <Button  target="_blank" href={link}  size="large" variant="contained" color="primary"><WhiteTextTypography color="secondary"
+                variant="h3">zum Projekt</WhiteTextTypography>  </Button> 
+      </CardActions>        
+      </CardContent>
+     
+    </Card>
+</Grid>
+        
     </div>  
     
 
